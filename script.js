9a97f7a7cf4e5d4a4cb58a3208412f9c96a46968
Praveen Kumar <praveenamnengnr@gmail.com>
2025-07-04 22:10:23 +0530

    typeofanddynamictyping

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-// 02. DATA TYPES
-// Number
-// String
-// boolean
-// undefined
-// null
-// Symbol
-// BigInt
+// Dynamic typing
+
+let javaScript = true;
+
+javaScript = "yes";
+
+// I can change the variable easily
+
+// undefined variable and assign value
+let kid;
+kid = "mohith";
+
+// type of operator
+console.log(typeof kid);
