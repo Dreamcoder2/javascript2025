c1625f73a502848df7f7776210e99675766922f9
Praveen Kumar <praveenamnengnr@gmail.com>
2025-07-04 21:59:06 +0530

    datatypes

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-// 01. values and variables
-
-let js = "amazing";
-console.log(js);
-
-// declare a variable
-let firstname = "praveen";
-
-// Rules for create the variables
-let firstName = "jonas";
-
-// cant use
-// let 3years = 3;
-// let jons&matila = 'jm'
-// let new = 27;
-// let Person = 'jons';
-// let PI = 3.1415
+// 02. DATA TYPES
+// Number
+// String
+// boolean
+// undefined
+// null
+// Symbol
+// BigInt
