b0a27cbbfca6e110db218b70180dc688418dcd36
Praveen Kumar <praveenamnengnr@gmail.com>
2025-07-04 21:45:17 +0530

    values&variables

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-test;
+// 01. values and variables
+
+let js = "amazing";
+console.log(js);
+
+// declare a variable
+let firstname = "praveen";
+
+// Rules for create the variables
+let firstName = "jonas";
+
+// cant use
+// let 3years = 3;
+// let jons&matila = 'jm'
+// let new = 27;
+// let Person = 'jons';
+// let PI = 3.1415
