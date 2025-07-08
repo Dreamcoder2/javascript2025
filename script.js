456dd36d5b59d5759ffd74c6a3bdc0e718f9af33
Praveen Kumar <praveenamnengnr@gmail.com>
2025-07-08 21:51:37 +0530

    if_else

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
-// BASIC OPERATOR
+// IF ELSE STATEMENTS
 
-let birthYear = 2025;
+const age = 19;
+const isEnough = age >= 18;
 
-const agepraveen = birthYear - 1991;
-console.log(agepraveen);
-
-console.log(birthYear * 2, birthYear / 10, 2 ** 3);
-
-// concatinate string
-
-const firstName = "jonas";
-const lastName = "man";
-console.log(firstName + lastName);
-
-// Assignment Operator
-
-let x = 10 + 5;
-console.log(x);
-
-x += 10; // x = x + 10 ;
-x *= 4;
-x++; // x = x + 1;
-x--;
-
-console.log(x);
-
-// comparsion operator
-
-console.log(agepraveen > birthYear); // >,<,>=,<=
+if (isEnough) {
+  console.log("can start driving");
+} else {
+  console.log("cannot drivve");
+}
