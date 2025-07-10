b1754681cefc6f0bcaa0c5801284b23b5b8b6497
Praveen Kumar <praveenamnengnr@gmail.com>
2025-07-10 22:05:58 +0530

    equality_operators

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-// TRUTHY AND FALSE VALUES
+// EQUALITY OPERATORS
 
-// 5 FALSE VALUE : 0,'' , UNDEFINED , NULL , NaN
+const age = 18;
 
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean("praveen"));
-console.log(Boolean({}));
-console.log(Boolean(""));
+if (age === 18) console.log("You are a adult");
+
+// == DO TYPE CORRECTION
+const age1 = "18";
+
+if (age1 == 18) console.log("you are reached");
+
+// ELSE IF
+
+const favourite = 9;
+
+if (favourite === 8) {
+  console.log("not my fav");
+} else if (favourite === 9) {
+  console.log("fav number");
+} else {
+  console.log("no fav number");
+}
+
+// Not or opposite
+
+if (favourite !== 8) console.log("why not 9");
